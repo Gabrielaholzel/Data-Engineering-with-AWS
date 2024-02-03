a6ee13a36320b40b2f2183c69b0d32075ffe8a75
Gabriela Holzel <76816893+Gabrielaholzel@users.noreply.github.com>
2024-02-03 20:01:27 +0000

    Update etl.py with try-excepts, docstrings and annotations

--- Cloud-Data-Warehouses/Project/etl.py
+++ Cloud-Data-Warehouses/Project/etl.py
@@ -1,31 +1,56 @@
 import configparser
 import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import cursor, connection
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-def load_staging_tables(cur, conn):
+def load_staging_tables(cur: cursor, conn: connection) -> None:
+    """
+    Load data from S3 into staging tables on Redshift.
+
+    Args:
+    cur: Cursor object for the database connection.
+    conn: Connection object to the Redshift database.
+
+    Returns:
+    None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def insert_tables(cur, conn):
+def insert_tables(cur: cursor, conn: connection) -> None:
+    """
+    Inserts data into target tables.
+
+    Args:
+    cur: Cursor object for the database connection.
+    conn: Connection object to the Redshift database.
+
+    Returns:
+    None
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
+    try:
+        config = configparser.ConfigParser()
+        config.read('dwh.cfg')
     
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
-
-    conn.close()
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        cur = conn.cursor()
+        
+        load_staging_tables(cur, conn)
+        insert_tables(cur, conn)
+    
+        conn.close()
+    except Error as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
